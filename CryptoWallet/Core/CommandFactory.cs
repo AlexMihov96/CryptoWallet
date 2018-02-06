c58622886384cbdd11595ad71077a7c9aacfddf7
Alex <alexandarmihov1@gmail.com>
2018-02-06 10:37:03 +0200

    create recover, recieve operations implemented

--- CryptoWallet/Core/CommandFactory.cs
+++ CryptoWallet/Core/CommandFactory.cs
@@ -1,4 +1,7 @@
-﻿using CryptoWallet.Interfaces;
+﻿using System;
+using System.Globalization;
+using CryptoWallet.Interfaces;
+using HBitcoin.KeyManagement;
 using NBitcoin;
 
 namespace CryptoWallet.Core
@@ -8,6 +11,7 @@ namespace CryptoWallet.Core
         private readonly Network currentNetwork;
         private readonly IWriter writer;
         private readonly IReader reader;
+        private readonly string walletFilePath = @"Wallets\";
 
         public CommandFactory(IWriter writer, IReader reader, Network currentNetwork)
         {
@@ -21,10 +25,10 @@ namespace CryptoWallet.Core
             switch (input)
             {
                 case (int)WalletOperations.Operations.Create:
-                    this.CreateWallet();
+                    this.CreateWalletCommand();
                     break;
                 case (int)WalletOperations.Operations.Recover:
-                    this.RecoverWallet();
+                    this.RecoverCommand();
                     break;
                 case (int)WalletOperations.Operations.Balance:
                     this.GetBalance();
@@ -33,7 +37,7 @@ namespace CryptoWallet.Core
                     this.ShowHistory();
                     break;
                 case (int)WalletOperations.Operations.Receive:
-                    this.Receive();
+                    this.ReceiveCommand();
                     break;
                 case (int)WalletOperations.Operations.Send:
                     this.Send();
@@ -59,9 +63,8 @@ namespace CryptoWallet.Core
             return this.reader.ReadLine();
         }
 
-        private void CreateWallet()
+        private void CreateWalletCommand()
         {
-            string walletFilePath = @"Wallets\";
             string password = string.Empty;
             string passwordConfirmation = string.Empty;
 
@@ -79,6 +82,46 @@ namespace CryptoWallet.Core
                 }
 
             } while (password != passwordConfirmation);
+
+            bool failure = true;
+
+            while (failure)
+            {
+                try
+                {
+                    this.WriteCommand("Enter wallet name: ");
+                    string walletName = this.ReadLineCommand();
+
+                    Mnemonic mnemonic;
+                    string fileName = walletFilePath + walletName + ".json";
+                    Safe safe = Safe.Create(out mnemonic, password, fileName, this.currentNetwork);
+                    Console.WriteLine();
+                    this.WriteLineCommand("Wallet created successfully");
+                    this.WriteLineCommand("Write down the following mnemonic words.");
+                    this.WriteLineCommand("With the mnemonic words AND the password you can recover this wallet.");
+                    this.WriteLineCommand("");
+                    this.WriteLineCommand("---------------");
+                    this.WriteLineCommand(mnemonic.ToString());
+                    this.WriteLineCommand("---------------");
+                    this.WriteLineCommand("Write down and keep them SECURED, please keep your private keys. Only through them you can access your coins!");
+                    this.WriteLineCommand("");
+
+                    const int firstTenAddressesInWallet = 10;
+                    for (int i = 0; i < firstTenAddressesInWallet; i++)
+                    {
+                        this.WriteLineCommand($"Address: {safe.GetAddress(i)} -> Private key: " +
+                                              $"{safe.FindPrivateKey(safe.GetAddress(i))}");
+                    }
+
+                    failure = false;
+                }
+                catch
+                {
+                    this.WriteLineCommand("");
+                    this.WriteLineCommand("Wallet already exists");
+                    this.WriteLineCommand("");
+                }
+            }
         }
 
         private void GetBalance()
@@ -91,9 +134,35 @@ namespace CryptoWallet.Core
             throw new System.NotImplementedException();
         }
 
-        private void Receive()
+        private void ReceiveCommand()
         {
-            throw new System.NotImplementedException();
+            this.WriteCommand("Enter wallet's name: ");
+            string walletName = this.ReadLineCommand();
+
+            this.WriteCommand("Enter password: ");
+            string password = this.ReadLineCommand();
+
+            this.Receive(password, walletName);
+        }
+
+        private void Receive(string password, string walletName)
+        {
+            try
+            {
+                Safe loadedSafe = Safe.Load(password, this.walletFilePath + walletName + ".json");
+
+                const int firstTenAddressesInWallet = 10;
+                for (int i = 0; i < firstTenAddressesInWallet; i++)
+                {
+                    this.WriteLineCommand($"{i + 1}: " + loadedSafe.GetAddress(i));
+                }
+            }
+            catch (Exception)
+            {
+                this.WriteLineCommand("");
+                this.WriteLineCommand("Wallet with such name does not exist!");
+                this.WriteLineCommand("");
+            }
         }
 
         private void ShowHistory()
@@ -101,9 +170,38 @@ namespace CryptoWallet.Core
             throw new System.NotImplementedException();
         }
 
-        private void RecoverWallet()
+        private void RecoverCommand()
         {
-            throw new System.NotImplementedException();
+            this.WriteLineCommand("Please not that the wallet cannot check if your password is correct or not. " +
+                                  "If you provide a wrong password a wallet will be recovered with your " +
+                                  "provided mnemonic and password pair: ");
+            this.WriteLineCommand("");
+
+            this.WriteCommand("Enter password: ");
+            string password = this.ReadLineCommand();
+
+            this.WriteCommand("Enter your 12 words phrase: ");
+            string mnemonic = this.ReadLineCommand();
+
+            this.WriteCommand("Enter date (yyyy-MM-dd): ");
+            string date = this.ReadLineCommand();
+
+            Mnemonic mnem = new Mnemonic(mnemonic);
+
+            this.Recover(password, mnem, date);
+        }
+
+        private void Recover(string password, Mnemonic mnem, string date)
+        {
+            Random random = new Random();
+
+            Safe safe = Safe.Recover(mnem, password,
+                this.walletFilePath + "RecoveredWalletNum" + random.Next() + ".json",
+                this.currentNetwork,
+                DateTimeOffset.ParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture));
+
+            this.WriteLineCommand("Wallet successfully recovered");
+            this.WriteLineCommand("");
         }
     }
 }
